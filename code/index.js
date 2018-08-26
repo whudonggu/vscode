46aa39f9cea6c4f5a05e062b39f698775e302acd
yudonggu <whu_yudonggu@whu.edu.cn>
2018-08-26 10:39:47 +0800

    test

---
+++ code/index.js
@@ -0,0 +1,8 @@
+//同步测试
+$(function () {
+    var mySwiper = new mySwiper('.swiper-container1', {/*轮播*/
+        autoplay: 2000,
+        pagination: '.swiper-pagination',
+        loop:true,
+    })
+})
